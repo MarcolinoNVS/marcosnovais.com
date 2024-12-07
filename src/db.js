a656d46c81bc218b64b4ac69c1aae05838c7e485
MarcolinoNVS <145811157+MarcolinoNVS@users.noreply.github.com>
2024-12-06 23:13:26 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2/promise");
 // Configuração do pool de conexões
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
+  user: "****",
   password: "****", 
-  database: "bd_novais",
+  database: "****",
   port: 3306, 
   waitForConnections: true,
   connectionLimit: 10,
